5f986a5dab0949ca1f775a18b948a73149264b8a
treecon <kon3dos@gmail.com>
2023-11-15 01:59:49 +0200

    api base URL

--- src/app/core/services/http/photos.service.ts
+++ src/app/core/services/http/photos.service.ts
@@ -11,8 +11,14 @@ export class PhotosService {
 
   constructor(private httpClient: HttpClient) { }
 
+  // this is wrong practice but is done due to deployment issues
+  // (no web server is set to redirect /api requests)
+  // also, i have not yet found a way to proxy requests in production (only in development server)
+  // an interceptor could be a solution
+  baseURL = 'http://85.208.48.165:3005';
+
   getPhotos(size: number, isPublic = false) {
-    const url = isPublic ? `/api/photo/public?size=${size}` : `/api/photo?size=${size}`;
+    const url = isPublic ? `${this.baseURL}/api/photo/public?size=${size}` : `/api/photo?size=${size}`;
 
     return this.httpClient.get<ServerResponse<Photo[]>>(url)
       .pipe(take(1));
@@ -24,12 +30,12 @@ export class PhotosService {
     // formData.append('photo', new Blob([photo]), filename);
     formData.append('photo', photo);
 
-    return this.httpClient.post<ServerResponse<null>>('/api/photo', formData)
+    return this.httpClient.post<ServerResponse<null>>(`${this.baseURL}/api/photo`, formData)
       .pipe(take(1));
   }
 
   vote(photoID: string, type: 'UP' | 'DOWN') {
-    return this.httpClient.post<ServerResponse<null>>(`/api/photo/${photoID}/${type.toLowerCase()}`, null)
+    return this.httpClient.post<ServerResponse<null>>(`${this.baseURL}/api/photo/${photoID}/${type.toLowerCase()}`, null)
       .pipe(take(1));
   }
 }
