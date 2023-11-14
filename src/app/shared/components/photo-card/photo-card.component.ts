cd7a10682c6fce465dbfba257048a00268c77f6a
treecon <kon3dos@gmail.com>
2023-11-14 16:41:45 +0200

    WIP

--- src/app/shared/components/photo-card/photo-card.component.ts
+++ src/app/shared/components/photo-card/photo-card.component.ts
@@ -9,7 +9,7 @@ import { PhotosService } from 'src/app/core/services/http/photos.service';
 })
 export class PhotoCardComponent {
   @Input() size: 'md' | 'lg' = 'md';
-  @Input() isWinningPhoto = false;
+  @Input() contestPosition: number | null = null;
   @Input() isWithActions: boolean = false;
   @Input() data!: Photo;
 
