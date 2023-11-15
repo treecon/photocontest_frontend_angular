731cacdb67616eca4ecc9736f05e93a40dd7e6de
treecon <kon3dos@gmail.com>
2023-11-15 02:15:06 +0200

    WIP

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
     keycloak: {
         // url: 'http://localhost:8081/keycloak',
-        url: 'http://85.208.48.165:8090/keycloak/',
+        url: 'http://85.208.48.165:8090/keycloak',
         realm: 'photocontest',
         clientId: 'frontend',
     }
